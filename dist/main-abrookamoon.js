6bb6305f026b9e41f90b64cf9f6b85be3c3b32da
Aleksandar Veljkovic <aleksandar.veljkovic@diwanee.com>
2016-09-09 09:43:11 +0200

    fullmodal for abrookamoon...

--- dist/main-abrookamoon.js
+++ dist/main-abrookamoon.js
@@ -4397,6 +4397,82 @@ if ( typeof module != 'undefined' && module.exports ) {
 })(window, document, Math);
 define("iscroll", function(){});
 
+;
+
+/*
+ *  jQuery OwlCarouselRtl v.0.9.0
+ *
+ *  Copyright (c) 2015 Aleksandar Veljkovic
+ *  https://github.com/biosonic/FullModal.git
+ *
+ *  Licensed under MIT
+ *
+ */
+
+(function($) {
+
+  $.fn.fullModal = function(set) {
+
+    set = set || {};
+    set.onclose = set.onclose || function() {
+    };
+    set.closeText = set.closeText || "<span>&#215;</span>";
+    set.aditionalStyle = set.aditionalStyle || false;
+    set.closeButton = set.closeButton || ((set.closeButton===false) ? false : true);
+
+    var toReturn = {};
+
+    var $content = $(this);
+    var $fullScreenWrap = $('<div id="dwf-fullscreen-wrap" />');
+    var $exodusSource = $('<div id="dfs-exodus-source" />');
+    var $closeButton = $("<a href='#' class='dfs-close-button'>" + set.closeText + "</a>");
+    var $headStyle = $("<style class='full-screan-style'>body > * {max-height:0!important;overflow:hidden!important;margin:0!important;padding:0!important;display:none!important;} body > #dwf-fullscreen-wrap,.chromeperfectpixel-overlay {max-height:none!important;display:block!important;} body, html {height:100%;}</style>");
+    var scrollBack = $(window).scrollTop();
+
+    $content.after($fullScreenWrap);
+    $fullScreenWrap.append($content);
+
+    $fullScreenWrap.after($exodusSource);
+    $exodusSource.append($fullScreenWrap);
+
+    if (set.closeButton === true) {
+      $fullScreenWrap.append($closeButton);
+    }
+
+    $('head').append($headStyle);
+
+    $("html").addClass('dwf-fullscreen');
+    $("body").prepend($fullScreenWrap);
+
+
+    if (set.aditionalStyle) {
+      $('head').append("<style class='full-screan-style-aditional'>" + set.aditionalStyle + "</style>");
+    }
+
+    toReturn.close = function() {
+      $exodusSource.after($content);
+      $exodusSource.remove();
+      $fullScreenWrap.remove();
+      $headStyle.remove();
+      $(".full-screan-style-aditional").remove();
+      $("html").removeClass('dwf-fullscreen');
+      $(window).scrollTop(scrollBack);
+      set.onClose();
+    };
+
+    $closeButton.click(function(e) {
+      e.preventDefault();
+      toReturn.close();
+    });
+
+    return toReturn;
+
+  };
+
+})(jQuery);
+
+define("fullScreen", function(){});
+
 /*!
  *  Sharrre.com - Make your sharing widget!
  *  Version: beta 1.3.5
@@ -5757,9 +5833,6 @@ window.backboneApp.build = "";
   define("mediaGallery", [], function () {
     return {};
   });
-  define("fullScreen", [], function () {
-    return {};
-  });
   
 
   require.config({
@@ -5768,7 +5841,7 @@ window.backboneApp.build = "";
       underscore: '../bower_components/underscore/underscore-min',
       backbone: '../bower_components/backbone/backbone',
       text: '../bower_components/requirejs-text/text',
-      //fullScreen: '../bower_components/fullmodal/full-modal',
+      fullScreen: '../bower_components/fullmodal/full-modal',
       caption: '../bower_components/gallery-captions/gallery-caption',
       //slick: '../bower_components/slick.js/slick/slick',
       iscroll: '../bower_components/iscroll/build/iscroll',
