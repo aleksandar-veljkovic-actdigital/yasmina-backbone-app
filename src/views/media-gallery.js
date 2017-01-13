c793ee77327718482bf373737a9a6635454fecd8
Aleksandar Veljkovic <aleksandar.veljkovic@diwanee.com>
2017-01-13 16:29:09 +0100

    WHATSUPP GA BY DOCUMENT.EVENT

--- src/views/media-gallery.js
+++ src/views/media-gallery.js
@@ -331,7 +331,8 @@ define([
         },
         click: function(api, options) {
           window.location.href = options.text;
-          _gaq.push(['_trackEvent', 'smart-whats-app-share', 'click', 'share']);
+          $(document).trigger("gallerySharrreClick");
+          $(document).trigger("gallerySharrreClickWhatsapp");
         }
       });
     },
