f79a42209036809bcef3fb72ddbed83c877b61d5
Aleksandar Veljkovic <aleksandar.veljkovic@diwanee.com>
2017-05-16 10:16:42 +0200

    yasmina :: slick carosel excluded from bild

--- src/main-yasmina.js
+++ src/main-yasmina.js
@@ -14,18 +14,22 @@ window.backboneApp.build = "";
   });
   define("owl", [], function() {
     return jQuery.fn.owlCarousel;
-  });  
+  });
   define("owlRtl", [], function() {
     return jQuery.fn.owlCarouselRtl;
-  }); 
+  });
   define("icheck", [], function() {
     return jQuery.fn.iCheck;
-  }); 
+  });
   define("sharrre", [], function() {
     return jQuery.fn.sharrre;
-  });  
+  });
+  define("slick", [], function() {
+    return jQuery.fn.sharrre;
+  });
 
   require.config({
+    waitSeconds : 100,
     //urlArgs: "bust=" + (new Date()).getTime(),
     paths: {
       underscore: '../bower_components/underscore/underscore-min',
@@ -33,14 +37,14 @@ window.backboneApp.build = "";
       text: '../bower_components/requirejs-text/text',
       fullScreen: '../bower_components/fullmodal/full-modal',
       caption: '../bower_components/gallery-captions/gallery-caption',
-      slick: '../bower_components/slick.js/slick/slick',
+      //slick: '../bower_components/slick.js/slick/slick',
       iscroll: '../bower_components/iscroll/build/iscroll',
       maxDimensionPercentage: '../bower_components/max-dimension-percentage/max-dimension-percentage',
       //sharrre: '../bower_components/sharrre/jquery.sharrre',
       //icheck: '../bower_components/iCheck/icheck',
       //owl: '../bower_components/owlcarousel/owl-carousel/owl.carousel.min',
       //owlRtl: '../bower_components/owlcarouselrtl/owl.carousel.rtl'
-            
+
       poll: 'views/poll',
       mediaGallery: 'views/media-gallery',
       mediaGalleryBranded: 'views/media-gallery-branded',
