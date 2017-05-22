7fb9f14286a38e68d064dd539dfb6156732c6a28
Aleksandar Veljkovic <aleksandar.veljkovic@diwanee.com>
2017-05-22 14:36:31 +0200

    yasmina :: iscol on site

--- src/main-yasmina.js
+++ src/main-yasmina.js
@@ -27,6 +27,9 @@ window.backboneApp.build = "";
   define("slick", [], function() {
     return jQuery.fn.sharrre;
   });
+  define("iscroll", [], function() {
+    return jQuery.fn.iscroll;
+  });
 
   require.config({
     waitSeconds : 100,
@@ -38,7 +41,7 @@ window.backboneApp.build = "";
       fullScreen: '../bower_components/fullmodal/full-modal',
       caption: '../bower_components/gallery-captions/gallery-caption',
       //slick: '../bower_components/slick.js/slick/slick',
-      iscroll: '../bower_components/iscroll/build/iscroll',
+      //iscroll: '../bower_components/iscroll/build/iscroll',
       maxDimensionPercentage: '../bower_components/max-dimension-percentage/max-dimension-percentage',
       //sharrre: '../bower_components/sharrre/jquery.sharrre',
       //icheck: '../bower_components/iCheck/icheck',
